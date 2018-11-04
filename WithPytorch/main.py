caeaabff6167a3a48b624de160535e6b7d8cc955
hob201801 <sdernal2@gmail.com>
2018-11-04 15:09:06 +0300

    Добавил обучение сети

--- WithPytorch/main.py
+++ WithPytorch/main.py
@@ -6,15 +6,15 @@ from model import Decoder, Encoder
 
 EMBEDDING_SIZE = 10
 HIDDEN_SIZE = 100
-
-# loader = DataLoader(10)
-# loader.parse_person_corpus('../Person-1000/collection')
-# loader.parse_rdf_corpus('../../corpus_for_pakhomov_2')
+print(abspath('./'))
+loader = DataLoader(10)
+loader.parse_person_corpus(abspath('../Persons-1000/collection'))
+loader.parse_rdf_corpus(abspath('../../corpus_for_pakhomov_2'))
 
 ep = EntriesProcessor(20,40)
-# ep.process(loader.entries)
-voc_size = 10
+ep.process(loader.entries)
+voc_size = ep.symbols_counter
 encoder = Encoder(input_size=voc_size, hidden_size=HIDDEN_SIZE, embedding_size=EMBEDDING_SIZE)
 decoder = Decoder(hidden_size=HIDDEN_SIZE, embedding_size=EMBEDDING_SIZE, output_size=voc_size, max_length=40)
 trainer = Trainer(encoder,decoder,ep)
-trainer.test_training()
+trainer.train()
--- WithPytorch/trainer.py
+++ WithPytorch/trainer.py
@@ -2,8 +2,14 @@ import random
 from torch import optim
 import torch
 import torch.nn as nn
+from torch.autograd import Variable
+import time
+
+from entriesprocessor import EntriesProcessor
+import numpy as np
+
 class Trainer:
-    def __init__(self, encoder, decoder, entries, teacher_forcing_ratio = 0.5, learning_rate=0.01,
+    def __init__(self, encoder, decoder, entries: EntriesProcessor , teacher_forcing_ratio = 0.5, learning_rate=0.01,
                  max_input_length = 40, max_output_length = 20):
         self.encoder = encoder
         self.decoder = decoder
@@ -70,3 +76,49 @@ class Trainer:
         input_tensor = torch.ones(self.max_input_length, BATCH_SIZE, 1, dtype=torch.long)
         output_tensor = torch.ones(self.max_output_length, BATCH_SIZE, dtype=torch.long)
         self.train_on_batch(input_tensor, output_tensor)
+
+    def iteate_batches(self, batch_size=100):
+        n_samples = self.entries.X_data.shape[0]
+        x_len = self.entries.X_data.shape[1]
+        y_len = self.entries.y_data.shape[1]
+
+        indices = np.arange(n_samples)
+        np.random.shuffle(indices)
+
+        for start in range(0, n_samples, batch_size):
+            end = min(start + batch_size, n_samples)
+            batch_idx = indices[start:end]
+
+            X_batch = self.entries.X_data[batch_idx, ]
+            Y_batch = self.entries.y_data[batch_idx, ]
+
+            X_batch = torch.from_numpy(X_batch)
+            Y_batch = torch.from_numpy(Y_batch)
+
+            X_batch =  X_batch.type(torch.LongTensor)
+            Y_batch = Y_batch.type(torch.LongTensor)
+
+            X_batch = X_batch.view(x_len, -1, 1)
+            Y_batch = Y_batch.view(y_len, -1)
+            yield Variable(X_batch), Variable(Y_batch)
+
+    def train_epoch(self):
+        '''
+        Train on dataset for one epoch
+        :return: None
+        '''
+        train_count, train_loss = 0, 0
+        start_time = time.time()
+        for i, (x_batch, y_batch) in enumerate(self.iteate_batches()):
+            loss = self.train_on_batch(x_batch, y_batch)
+            train_loss += loss
+            train_count = i + 1
+            current_time = time.time()
+            print('\r\tTrain Loss: %6f \t Time: %ds' % (train_loss / train_count, current_time - start_time), end='')
+        print()
+
+    def train(self):
+        print()
+        for epoch in range(1,6):
+            print('Epoch %d' % (epoch))
+            self.train_epoch()
